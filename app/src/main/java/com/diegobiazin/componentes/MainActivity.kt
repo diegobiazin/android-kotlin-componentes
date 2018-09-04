d04e94d515d800966849ab8fd914be4e1c52923c
Diego Biazin <diego_biazin@hotmail.com>
2018-09-03 21:57:33 -0300

    Toast Notification

--- app/src/main/java/com/diegobiazin/componentes/MainActivity.kt
+++ app/src/main/java/com/diegobiazin/componentes/MainActivity.kt
@@ -2,11 +2,27 @@ package com.diegobiazin.componentes
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setListeners()
+    }
+
+    override fun onClick(v: View) {
+        val id = v.id
+        if(id == R.id.buttonToastMe){
+            Toast.makeText(this, "Toast notification!", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun setListeners() {
+        buttonToastMe.setOnClickListener(this)
     }
 }
