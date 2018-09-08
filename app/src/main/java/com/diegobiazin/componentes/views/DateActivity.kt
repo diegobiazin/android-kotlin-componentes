90b1c29b970f9dd930344d596093e30d4c77dab0
Diego Biazin <diego_biazin@hotmail.com>
2018-09-08 15:57:44 -0300

    Timepicker - Eventos

--- app/src/main/java/com/diegobiazin/componentes/views/DateActivity.kt
+++ app/src/main/java/com/diegobiazin/componentes/views/DateActivity.kt
@@ -1,16 +1,19 @@
 package com.diegobiazin.componentes.views
 
 import android.app.DatePickerDialog
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.DatePicker
+import android.widget.TimePicker
+import android.widget.Toast
 import com.diegobiazin.componentes.R
 import kotlinx.android.synthetic.main.activity_date.*
 import java.text.SimpleDateFormat
 import java.util.*
 
-class DateActivity : AppCompatActivity(), View.OnClickListener, DatePickerDialog.OnDateSetListener {
+class DateActivity : AppCompatActivity(), View.OnClickListener, DatePickerDialog.OnDateSetListener, TimePicker.OnTimeChangedListener {
 
     private val mSimpleDateFormat = SimpleDateFormat("dd/MM/yyyy")
 
@@ -26,6 +29,25 @@ class DateActivity : AppCompatActivity(), View.OnClickListener, DatePickerDialog
 
         if (id == R.id.buttonDatepicker) {
             openDatepickerDialog()
+        } else if (id == R.id.buttonGetTime) {
+            if (Build.VERSION.SDK_INT >= 23) {
+
+                val value = "${timePicker.hour}:${timePicker.minute}"
+                Toast.makeText(this, value, Toast.LENGTH_LONG).show()
+            } else {
+
+                val value = "${timePicker.currentHour}:${timePicker.currentMinute}"
+                Toast.makeText(this, value, Toast.LENGTH_LONG).show()
+
+            }
+        } else if (id == R.id.buttonSetTime) {
+            if (Build.VERSION.SDK_INT >= 23) {
+                timePicker.hour = 20
+                timePicker.minute = 15
+            } else {
+                timePicker.currentHour = 20
+                timePicker.currentMinute = 15
+            }
         }
     }
 
@@ -38,8 +60,16 @@ class DateActivity : AppCompatActivity(), View.OnClickListener, DatePickerDialog
 
     }
 
+    override fun onTimeChanged(timePicker: TimePicker, hourOfDay: Int, minute: Int) {
+        Toast.makeText(this, "$hourOfDay:$minute", Toast.LENGTH_LONG).show()
+    }
+
     private fun setListeners() {
         buttonDatepicker.setOnClickListener(this)
+        buttonGetTime.setOnClickListener(this)
+        buttonSetTime.setOnClickListener(this)
+
+        timePicker.setOnTimeChangedListener(this)
     }
 
     private fun openDatepickerDialog() {
